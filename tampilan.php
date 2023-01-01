a16a59934a141ef9e174c26d07ac3de208d727aa
romzyyy <fijay0117@gmail.com>
2023-01-02 00:53:02 +0700

    change database koneksi

--- tampilan.php
+++ tampilan.php
@@ -1,74 +1,84 @@
+<h3>Tambah siswa</h3>
+
+<form action="" method="post">
+    <table>
+        <tr>
+            <td width="130px">NIM </td>
+            <td><input type="text" name="nim"></td>
+        </tr>
+        <tr>
+            <td width="130px">Nama </td>
+            <td><input type="text" name="nama"></td>
+        </tr>
+        <tr>
+            <td width="130px">Alamat</td>
+            <td><input type="text" name="alamat"></td>
+        </tr>
+        <tr>
+            <td width="130px">Telp</td>
+            <td><input type="text" name="telp"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" value="Simpan" name="proses"></td>
+        </tr>
+    </table>
+</form>
+
 <h1>Data siswa</h1>
 
 <table border="1">
     <tr>
         <th>NO</th>
+        <th>NIM</th>
         <th>Nama</th>
-        <th>Kelas</th>
         <th>Alamat</th>
+        <th>Telp</th>
         <th colspan="2">Aksi</th>
     </tr>
 
-    <!-- <?php
+    <?php
     include "koneksi.php";
     $no = 1;
-    $ambildata = mysqli_query($koneksi, "select * from siswa");
+    $ambildata = mysqli_query($koneksi, "select * from mahasiswa");
     while ($tampil = mysqli_fetch_array($ambildata)){
         echo "
         <tr>
         <td>$no</td>
-        <td>$tampil[nama_siswa]</td>
-        <td>$tampil[kelas]</td>
-        <td>$tampil[alamat_siswa]</td>
-        <td><a href='?kode=$tampil[id]'>Hapus</a></td>
-        <td><a href='ubah_siswa.php?kode=$tampil[id]'>Ubah</a></td>
+        <td>$tampil[nim]</td>
+        <td>$tampil[nama]</td>
+        <td>$tampil[alamat]</td>
+        <td>$tampil[telp]</td>
+        <td><a href='?kode=$tampil[nim]'>Hapus</a></td>
+        <td><a href='ubah_siswa.php?kode=$tampil[nim]'>Ubah</a></td>
         </tr>";
         $no++;
     }
-    ?> -->
-    <?php
-    include "koneksi.php";
-    $no = 1;
-    $ambildata = mysqli_query($koneksi, "select * from siswa") or die (mysqli_error($koneksi));
-    do { 
-        echo "
-        <tr>
-        <td>$no</td>
-        <td>$tampil[nama_siswa]</td>
-        <td>$tampil[kelas]</td>
-        <td>$tampil[alamat_siswa]</td>
-        <td><a href='?kode=$tampil[id]'>Hapus</a></td>
-        <td><a href='ubah_siswa.php?kode=$tampil[id]'>Ubah</a></td>
-        </tr>";  
-        $no++;
-    }while($tampil = mysqli_fetch_array($ambildata)); 
     ?>
-    <!-- <?php
-    include "koneksi.php";
-    $no = 1;
-    $ambildata = mysqli_query($koneksi, "select * from siswa") or die (mysqli_error($koneksi));
-    for ( ;  $tampil = mysqli_fetch_array($ambildata);) {
-        echo "
-        <tr>
-        <td>$no</td>
-        <td>$tampil[nama_siswa]</td>
-        <td>$tampil[kelas]</td>
-        <td>$tampil[alamat_siswa]</td>
-        <td><a href='?kode=$tampil[id]'>Hapus</a></td>
-        <td><a href='ubah_siswa.php?kode=$tampil[id]'>Ubah</a></td>
-        </tr>";
-        $no++; 
-    };
-    ?> -->
+
 </table>
+<?php
+include "koneksi.php";
+$sql = mysqli_query($koneksi, "select * from mahasiswa where nim='$_GET[kode]'");
+$data = mysqli_fetch_array($sql)
+?>
+
+
 
 <?php
 include "koneksi.php";
+if(isset($_POST['proses'])){
+    mysqli_query($koneksi,"insert into mahasiswa set 
+    nim = '$_POST[nim]',
+    nama = '$_POST[nama]',
+    alamat = '$_POST[alamat]',
+    telp = '$_POST[telp]'");
+    header("location:tampilan.php");
+}
 if(isset($_GET['kode'])){
-    mysqli_query($koneksi, "delete from siswa where id='$_GET[kode]'");
-
+    mysqli_query($koneksi, "delete from mahasiswa where nim='$_GET[kode]'");
     echo "Data telah terhapus";
-    echo "<meta http-equiv=refresh content=2;URL='tampilan.php'";
+    header("location:tampilan.php");
 }
 
 ?>
--- ubah_siswa.php
+++ ubah_siswa.php
@@ -1,43 +1,45 @@
 <?php
 include "koneksi.php";
-$sql = mysqli_query($koneksi, "select * from siswa where id='$_GET[kode]'");
+$sql = mysqli_query($koneksi, "select * from mahasiswa where nim='$_GET[kode]'");
 $data = mysqli_fetch_array($sql)
 ?>
-
-<h3>Ubah siswa</h3>
+<h3>Ubah Mahasiswa</h3>
 
 <form action="" method="post">
     <table>
         <tr>
-            <td width="130px">Nama Siswa</td>
-            <td><input type="text" name="nama_siswa" value="<?php echo $data['nama_siswa']; ?>"></td>
+            <td width="130px">NIM</td>
+            <td><input type="text" name="nim" value="<?php echo $data['nim']; ?>"></td>
         </tr>
         <tr>
-            <td width="130px">Kelas</td>
-            <td><input type="text" name="kelas" value="<?php echo $data['kelas']; ?>"></td>
+            <td width="130px">Nama</td>
+            <td><input type="text" name="nama" value="<?php echo $data['nama']; ?>"></td>
         </tr>
         <tr>
             <td width="130px">Alamat</td>
-            <td><input type="text" name="alamat_siswa" value="<?php echo $data['alamat_siswa']; ?>"></td>
+            <td><input type="text" name="alamat" value="<?php echo $data['alamat']; ?>"></td>
+        </tr>
+        <tr>
+            <td width="130px">Telp</td>
+            <td><input type="text" name="telp" value="<?php echo $data['telp']; ?>"></td>
         </tr>
         <tr>
             <td></td>
-            <td><input type="submit" value="Simpan" name="proses"></td>
+            <td><input type="submit" value="Simpan" name="simpan"></td>
         </tr>
     </table>
-    <button><a href="index.php">Batalkan perubahan</a></button>
 </form>
 
 <?php
 include "koneksi.php";
 
-if(isset($_POST['proses'])){
-    mysqli_query($koneksi,"update siswa set
-    nama_siswa = '$_POST[nama_siswa]',
-    kelas = '$_POST[kelas]',
-    alamat_siswa = '$_POST[alamat_siswa]',
-    where id = '$_GET[kode]'");
-    echo "Data siswa baru telah di ubah";
-    echo "<meta http-equiv=refresh content=1;URL='tampilan.php'";
+if(isset($_POST['simpan'])){
+    mysqli_query($koneksi,"update mahasiswa set
+    nama = '$_POST[nama]',
+    alamat = '$_POST[alamat]',
+    telp = '$_POST[telp]'
+    where nim = '$_GET[kode]'");
+    echo "Data mahasiswa baru telah di ubah";
+    header("location:tampilan.php");
 }
 ?>
